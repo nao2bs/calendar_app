5989ce7792de09168612e55569ebf1df77888e4f
nao2bs <57845054+nao2bs@users.noreply.github.com>
2024-11-16 06:40:18 +0000

    CORSポート番号変更

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -8,7 +8,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'example.com'
-    origins 'http://localhost:5173'
+    # origins 'http://localhost:5173'
+    origins 'http://localhost:8080'
 
     resource '*',
              headers: :any,
