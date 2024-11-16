fa53131d22c2ac55edfeceadc969543d4c4b6d5b
nao2bs <57845054+nao2bs@users.noreply.github.com>
2024-11-16 07:38:45 +0000

    backendからフェッチする

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -9,7 +9,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'example.com'
     # origins 'http://localhost:5173'
-    origins 'http://localhost:8080'
+    # origins 'http://localhost:8080'
+    origins 'http://127.0.0.1:8080'
 
     resource '*',
              headers: :any,
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,9 +1,17 @@
-<template>
-  <p>Hello World!</p>
+<!-- <template>
+  <div>
+    <p>Hello World!</p>
+    <div>
+      <p>{{ message }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+  data: () => ({
+    message: 'Hello World!',
+  }),
 };
-</script>
+</script> -->
