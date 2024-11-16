8fffc0a9e95dc2945f1e976eb966363ea94957f6
nao2bs <57845054+nao2bs@users.noreply.github.com>
2024-11-16 07:53:47 +0000

    CalendarとDetailsでフェッチする

--- frontend/src/components/CalendarComponent.vue
+++ frontend/src/components/CalendarComponent.vue
@@ -3,15 +3,26 @@
     <h1>Calendar</h1>
     <p>events:</p>
     <p>{{ events }}</p>
+    <ul>
+      <li v-for="event in events" :key="event.id">
+        {{ event.name }}
+      </li>
+    </ul>
     <button type="submit" @click="fetchEvents()">fetchEvents</button>
+
+    <CalendarDetails />
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import CalendarDetails from "./CalendarDetails.vue";
 
 export default {
   name: "CalendarComponent",
+  components: {
+    CalendarDetails,
+  },
   data: () => ({
     events: [],
   }),
